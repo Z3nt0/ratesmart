d6db505eddc67c7e6bb78454096d913d2d05ce54
Z3nt0 <marquezanthonyjay@gmail.com>
2024-07-18 10:52:18 +0800

    major changes bugfix - signup, login buttons added sign-up-component

--- frontend/src/app/components/sign-up/sign-up.component.ts
+++ frontend/src/app/components/sign-up/sign-up.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,25 +6,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  user = {
+    name: '',
+    username: '',
+    password: ''
+  };
 
-  constructor(private http: HttpClient, private router: Router) { }
-
-  onSubmit(form: any) {
-    // Form submission logic
-    const formData = new FormData(form);
-
-    this.http.post('http://your-laravel-app.test/api/register', formData)
-      .subscribe(
-        (response) => {
-          console.log(response);
-          // Handle successful registration
-          this.router.navigate(['/']);
-        },
-        (error) => {
-          console.error(error);
-          // Handle registration errors
-        }
-      );
+  onSubmit() {
+    console.log('User signed up with: ', this.user);
+    // Add your sign-up logic here (e.g., sending data to the server)
   }
-
 }

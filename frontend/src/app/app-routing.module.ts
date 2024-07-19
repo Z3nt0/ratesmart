b784ddbe4e89fbf9df900438f16630dfcd7b7272
Z3nt0 <marquezanthonyjay@gmail.com>
2024-07-19 11:47:04 +0800

    add settings on app-routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component'; 
 import { AdminAboutComponent } from './components/admin-about/admin-about.component'; // Import AdminAboutComponent
+import { AdminSettingsComponent } from './components/admin-settings/admin-settings.component'; // Import AdminSettingsComponent
 
 const routes: Routes = [
   { path: '', redirectTo: '/splash', pathMatch: 'full' },
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin-dashboard', component: AdminDashboardComponent },
-  { path: 'admin-about', component: AdminAboutComponent } // Add route for AdminAboutComponent
+  { path: 'admin-about', component: AdminAboutComponent },
+  { path: 'admin-settings', component: AdminSettingsComponent } // Add route for AdminSettingsComponent
 ];
 
 @NgModule({

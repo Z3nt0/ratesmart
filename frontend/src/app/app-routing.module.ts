8508f47c2939ffad8105bdd727abc7654f49d736
angel4lalili <nicoleangelacenabre@gmail.com>
2024-07-19 11:56:49 +0800

    Added Admin-profile

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { SplashScreenComponent } from './components/splash-screen/splash-screen.
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component'; 
+import { AdminProfileComponent } from './components/admin-profile/admin-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/splash', pathMatch: 'full' },
   { path: 'splash', component: SplashScreenComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin-dashboard', component: AdminDashboardComponent } 
+  { path: 'admin-dashboard', component: AdminDashboardComponent } ,
+  { path: 'admin-profile', component: AdminProfileComponent  } 
 ];
 
 @NgModule({

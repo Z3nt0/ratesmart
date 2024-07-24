fd764bb30a9ff3e6ea22cbfbc652b3e45c11678e
Z3nt0 <marquezanthonyjay@gmail.com>
2024-07-24 13:02:06 +0800

    added admin-form-customization component

---
+++ frontend/src/app/components/admin-forms/admin-form-customize/admin-form-customize.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-form-customize',
+  templateUrl: './admin-form-customize.component.html',
+  styleUrls: ['./admin-form-customize.component.scss']
+})
+export class AdminFormCustomizeComponent {
+
+}

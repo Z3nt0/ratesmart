6fc411f8bf75443574a6f445a238f098db84b897
Z3nt0 <marquezanthonyjay@gmail.com>
2024-07-26 11:20:35 +0800

    Add AdminFormAnalyticsComponent to app.module.ts

---
+++ frontend/src/app/components/admin-forms/admin-form-analytics/admin-form-analytics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-form-analytics',
+  templateUrl: './admin-form-analytics.component.html',
+  styleUrls: ['./admin-form-analytics.component.scss']
+})
+export class AdminFormAnalyticsComponent {
+
+}

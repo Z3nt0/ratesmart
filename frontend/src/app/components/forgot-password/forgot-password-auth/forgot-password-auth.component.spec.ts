7b1e37f56c43b4df00d32b25e4bebaa7fb2f39b8
Z3nt0 <marquezanthonyjay@gmail.com>
2024-07-26 11:26:38 +0800

    Add ForgotPasswordAuthComponent to app module declarations

---
+++ frontend/src/app/components/forgot-password/forgot-password-auth/forgot-password-auth.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForgotPasswordAuthComponent } from './forgot-password-auth.component';
+
+describe('ForgotPasswordAuthComponent', () => {
+  let component: ForgotPasswordAuthComponent;
+  let fixture: ComponentFixture<ForgotPasswordAuthComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ForgotPasswordAuthComponent]
+    });
+    fixture = TestBed.createComponent(ForgotPasswordAuthComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

74615bbbb7d3817c89cb2ebf31773d87717e12b6
angel4lalili <nicoleangelacenabre@gmail.com>
2024-07-29 12:59:29 +0800

    minor changes-admin-form-customize-logo-component

--- frontend/src/app/components/admin-forms/admin-form-customize/admin-form-customize-logo/admin-form-customize-logo.component.ts
+++ frontend/src/app/components/admin-forms/admin-form-customize/admin-form-customize-logo/admin-form-customize-logo.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { SidenavService } from '../../../shared/sidenav/sidenav.service';
 @Component({
   selector: 'app-admin-form-customize-logo',
   templateUrl: './admin-form-customize-logo.component.html',
   styleUrls: ['./admin-form-customize-logo.component.scss']
 })
 export class AdminFormCustomizeLogoComponent {
+  constructor(private sidenavService: SidenavService) {}
 
+  openSidenav() {
+    this.sidenavService.toggle(); // Ensure SidenavService has a `toggle` method
+  }
 }

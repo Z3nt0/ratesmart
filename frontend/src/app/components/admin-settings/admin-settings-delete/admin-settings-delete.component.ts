d26400b15dc7b8eb5de8ab9d9fa8e7394394859f
angel4lalili <nicoleangelacenabre@gmail.com>
2024-08-01 12:05:46 +0800

    minor changes-admin-settings-delete

--- frontend/src/app/components/admin-settings/admin-settings-delete/admin-settings-delete.component.ts
+++ frontend/src/app/components/admin-settings/admin-settings-delete/admin-settings-delete.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { SidenavService } from '../../shared/sidenav/sidenav.service'; 
 
 @Component({
-  selector: 'app-admin-settings-delete',
+  selector: 'app-admin-settings',
   templateUrl: './admin-settings-delete.component.html',
   styleUrls: ['./admin-settings-delete.component.scss']
 })
 export class AdminSettingsDeleteComponent {
+  @Input() color: ThemePalette = 'primary'; // Default value
 
+  settings: any = {
+    notificationsEnabled: true,
+    theme: 'light'
+  };
+
+  toggleNotifications() {
+    if (this.settings) {
+      this.settings.notificationsEnabled = !this.settings.notificationsEnabled;
+    }
+  }
+
+  changeTheme(newTheme: string) {
+    if (this.settings) {
+      this.settings.theme = newTheme;
+    }
+  }
+  constructor(private sidenavService: SidenavService) {}
+
+  openSidenav() {
+    this.sidenavService.toggle();
+  }
 }
